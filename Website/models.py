7a1a672dd008c3c4f560f207c2ad88841a73dee6
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-17 14:57:13 +0300

    Initial commit

--- Website/models.py
+++ Website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Members(models.Model):
+    fname = models.CharField('Fisrt Name', max_length=50)
+    lname = models.CharField('Last Name', max_length=50)
+    email = models.EmailField(max_length=50)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=50)
+    
+    def __str__(self):
+        return self.fname
--- Website/urls.py
+++ Website/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.home,name='home.html'),
+    path('join/', views.join,name='join.html'),
 ]
